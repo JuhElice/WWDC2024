fff7a46f05904af1df42af4bbbbeb8b727499a75
Julia Elice <juliajurc@hotmail.com>
2024-02-02 10:29:47 -0300

    Test changes just to try commiting

--- MainScreenView.swift
+++ MainScreenView.swift
@@ -11,7 +11,7 @@ struct MainScreenView: View {
 
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("dgdsydgysetds")
             
             ScrollView {
                 LazyVGrid (columns: [GridItem(),GridItem()]) {
