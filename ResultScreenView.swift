6d30afcfae1d8c679ed9b657693125aaec67330e
Julia Elice <juliajurc@hotmail.com>
2024-02-20 07:42:58 -0300

    New ajustments

---
+++ ResultScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultScreenView.swift
+//  WWDC2024
+//
+//  Created by Julia Elice Jurczyszyn on 19/02/24.
+//
+
+import SwiftUI
+
+struct ResultScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultScreenView()
+}
---
+++ ShowingCanva.swift
@@ -0,0 +1,8 @@
+//
+//  ShowingCanva.swift
+//  WWDC2024
+//
+//  Created by Julia Elice Jurczyszyn on 19/02/24.
+//
+
+import Foundation
